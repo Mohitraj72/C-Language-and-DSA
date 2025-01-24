ccfbb413bbf90868bb25528d4facd7387a5bf309
Mohitraj72 <digitalmcsc12345@gmail.com>
2025-01-24 11:01:52 +0530

    'update'

---
+++ video 27/Q1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    // Create vector, declare
+    vector<int> v;
+    vector<int> v1(5, 1);
+
+    // Size and capacity
+    cout << "size of v:" << v.size() << endl;
+    cout << "Capacity of v:" << v.capacity() << endl;
+
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(10);
+    cout << "Size of v:" << v.size() << endl;
+    cout << "Capacity of v:" << v.capacity() << endl;
+
+    // update value
+    v[1] = 5;
+
+    cout << "Size of v:" << v1.size() << endl;
+    cout << "Capacity of v:" << v1.capacity() << endl;
+
+    v1.push_back(8);
+
+    cout << "Size of v:" << v1.size() << endl;
+    cout << "Capacity of v:" << v1.capacity() << endl;
+
+    return 0;
+}
