5e2f4429dc36243232eeaf24d847fc712a88cd97
Mohitraj72 <digitalmcsc12345@gmail.com>
2025-05-24 12:08:08 +0530

    'update'

--- video 32/Q1.cpp
+++ video 32/Q1.cpp
@@ -1,40 +1,35 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-    // create 2d vector 
-    int n,m;
+    // create 2d vector
+    int n, m;
     cout << "enter row and column";
     cin >> n >> m;
 
-
-    vector<vector<int> >matrix(3,vector<int>(4,1));
+    vector<vector<int>> matrix(3, vector<int>(4, 1));
 
     // for(int i = 0; i < 3; i++)
     // for (int j = 0; j <4 ; j++)
     // cout << matrix [i][j]<< " ";
 
-    
     // cout << "Row = " << matrix.size();
     // cout<<endl;
     // cout<<"cols = " <<matrix[0].size();
 
     for (int i = 0; i < n; i++)
-    for (int j = 0; j <m; j++)
-    cin >> matrix[i][j];
+        for (int j = 0; j < m; j++)
+            cin >> matrix[i][j];
 
-    for (int i = 0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
-        cout << matrix[i][j] << " ";
+            cout << matrix[i][j] << " ";
         cout << endl;
     }
-
-
-    
-    
 }
